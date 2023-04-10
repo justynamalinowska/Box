5123eba0ef76b745f2ab8c491f59fd9cca08a3db
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-04-10 21:28:29 +0200

    add extension method “Kompresuj”

---
+++ Pudełko/Kompresuj.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Pudełko;
+
+namespace ExtensionMethod
+{
+	public static class BoxExtension
+	{
+		public static Pudelko Kompresuj(this Pudelko pudelko)
+		{
+			double edge = Math.Cbrt(pudelko.Objetosc);
+			return new Pudelko(edge, edge, edge);
+		}
+
+		
+
+	}
+}
+
--- Pudełko/Pudelko.cs
+++ Pudełko/Pudelko.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using ExtensionMethod;
 
 namespace Pudełko
 {
