3e9256273ca565ed5c08d950da04b52549b23f7d
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-04-22 11:58:40 +0200

    add code to confirm the functioning of individual implementations

--- Pudełko/Program.cs
+++ Pudełko/Program.cs
@@ -15,50 +15,72 @@ namespace Pudełko
     {
         public static void Main(string[] args)
         {
-            Pudelko p1 = new Pudelko(2.5, 9.321, 0.1);
+            Pudelko p1 = new Pudelko(54, 54, 54, UnitOfMeasure.centimeter);
             Console.WriteLine(p1.ToString());
-            Console.WriteLine(p1.ToString("m"));
-            Console.WriteLine(p1.ToString("cm"));
-            Console.WriteLine(p1.ToString("mm"));
-            Console.WriteLine(p1.Objetosc);
-            Console.WriteLine(p1.Pole);
 
-            Pudelko p2 = new Pudelko(1, 3.05, 2.1);
-            Pudelko p3 = new Pudelko(2.1, 1, 3.05);
-            Pudelko p4 = new Pudelko(2100, 1000, 3050, UnitOfMeasure.milimeter);
-            Pudelko p5 = new Pudelko(210, 100, 305, UnitOfMeasure.centimeter);
+            Pudelko p2 = new Pudelko(0.7, 42, unitOfMeasure: UnitOfMeasure.centimeter);
+            Console.WriteLine(p2.ToString());
 
-            Console.WriteLine(p2.Equals(p5));
-            Console.WriteLine(p2.Equals(p4));
-            Console.WriteLine(p2==p5);
-            Console.WriteLine(p2!=p4);
-            Console.WriteLine(p3);
-            Console.WriteLine(p4);
-            Console.WriteLine(p5);
-            Console.WriteLine(p4 + p5);
-            Console.WriteLine((p4+p5).Objetosc);
+            Pudelko p3 = new Pudelko(19.3, 9, 15, UnitOfMeasure.centimeter);
+            Console.WriteLine(p3.ToString());
 
-            Pudelko p6 = (2100, 1000, 3050);
-            Console.WriteLine(p6);
-            double[] tablica = (double[])p6;
-            Console.WriteLine(tablica[0] + " " + tablica[1] + " " + tablica[2]);
+            Pudelko p4 = new Pudelko(4, 5, 6);
+            Console.WriteLine(p4.ToString());
 
-            Console.WriteLine(p6[0]);
-            foreach (var x in p6)
-            {
-                Console.WriteLine(x);
-            }
+            Pudelko p5 = new Pudelko(2, 3, 1, UnitOfMeasure.meter);
+            Console.WriteLine(p5.ToString());
 
-            Pudelko p7 = new Pudelko(2.5, 9.321);
-            Console.WriteLine(p7.ToString());
+            Pudelko p10 = new Pudelko(3, 1, 2, UnitOfMeasure.meter);
+            Console.WriteLine(p10.ToString());
 
-            Pudelko p10 = new Pudelko(1, 2, 3);
-            Pudelko p20 = new Pudelko(4, 5, 6);
+            //ToString
+            Console.WriteLine();
+            Console.WriteLine("ToString");
+            Console.WriteLine(p5.ToString("cm"));
+            Console.WriteLine(p5.ToString("mm"));
+            Console.WriteLine(p1.ToString(null));
 
-            Console.WriteLine(p10+p20);
 
-            Pudelko pudelko = new Pudelko(2, 3, 4);
-            Console.WriteLine(pudelko.Kompresuj());
+            //Pole i Objętość
+            Console.WriteLine();
+            Console.WriteLine("Pole i objętość");
+            Console.WriteLine("P = " + p1.Pole);
+            Console.WriteLine("V = " + p1.Objetosc);
+
+
+            //Equals
+            Console.WriteLine();
+            Console.WriteLine("Równość pudełek");
+            Console.WriteLine(p5 == p10);
+            Console.WriteLine(p1.Equals(p2));
+
+
+            //dodawanie
+            Console.WriteLine();
+            Console.WriteLine("Dodawanie pudełek");
+            Console.WriteLine(p1 + p2);
+
+
+            //indexer i iterator
+            Console.WriteLine();
+            Console.WriteLine("Indexer i iterator");
+            Console.WriteLine("p2[1] = " + p2[1]);
+            Console.WriteLine("p3: ");
+            foreach (var item in p3)
+                Console.WriteLine(item);
+
+
+            //parsowanie
+            Console.WriteLine();
+            Console.WriteLine("Parsowanie");
+            Pudelko p7 = Pudelko.Parse("250.0 cm × 932.1 cm × 10.0 cm");
+            Console.WriteLine(p5.ToString("cm"));
+
+            //kompresja
+            Console.WriteLine();
+            Console.WriteLine("Kompresja");
+            Pudelko p8 = p7.Kompresuj();
+            Console.WriteLine(p8.ToString());
 
             List<Pudelko> boxes = new List<Pudelko>();
             boxes.Add(new Pudelko(2, 3, 9));
@@ -74,18 +96,16 @@ namespace Pudełko
 
             boxes.Sort(delegate (Pudelko p1, Pudelko p2)
             {
-                if (p1.Objetosc < p2.Objetosc) return -1;
-                else if (p1.Objetosc > p2.Objetosc) return 1;
-
+                if (p1.Objetosc != p2.Objetosc)
+                    return (p1.Objetosc.CompareTo(p2.Objetosc));
                 else
                 {
-                    if (p1.Pole < p2.Pole) return -1;
-                    else if (p1.Pole > p2.Pole) return 1;
-
+                    if(p1.Pole!=p2.Pole)
+                        return (p1.Pole.CompareTo(p2.Pole));
                     else
                     {
-                        if ((p1.A + p1.B + p1.C) < (p2.A + p2.B + p2.C)) return -1;
-                        else if ((p1.A + p1.B + p1.C) > (p2.A + p2.B + p2.C)) return 1;
+                        if((p1.A + p1.B + p1.C) != (p2.A + p2.B + p2.C))
+                            return(p1.A + p1.B + p1.C).CompareTo(p2.A + p2.B + p2.C);
                         else return 0;
                     }
                 }
